167e0e1ae8de8b1594a27b53c58b2d105356c14a
Rob Hirschfeld <rob@zehicle.com>
2010-01-23 10:23:06 -0800

    simulator working after fixing bus in cache read to include :raw.  To use simulator, 'rake sim:move'

--- webapp/app/controllers/feed_controller.rb
+++ webapp/app/controllers/feed_controller.rb
@@ -4,8 +4,10 @@ class FeedController < ApplicationController
 
   def bus
 
-    cache = Rails.cache.read(params[:id])
+    cache = Rails.cache.read params[:id], :raw => true
     data = "#{params[:lat]},#{params[:lng]}"
+    
+    logger.debug { "cache for '#{params[:id]}' was '#{cache}' will be '#{data}'" }
   
     # write to change, unless data has not chaned
     Rails.cache.write params[:id], data, :raw=>:true, :unless_exist => false, :expires_in => 5.minutes unless cache == data
